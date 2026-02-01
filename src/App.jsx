1643cea5cee83113b66322b02cd35c6f2d753a9a
Jasbir Singh <jasbir212singh@gmail.com>
2026-02-01 13:03:22 +0530

    added auth page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import AuthPage from "./pages/AuthPage/AuthPage.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </>
   );
