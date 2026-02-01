d36889bd526f8a58e5e19dac10ad1faf0f3ad08f
Jasbir Singh <jasbir212singh@gmail.com>
2026-02-01 15:35:13 +0530

    removed styling from app link

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -14,12 +14,7 @@ const AuthPage = () => {
             <Box textAlign={"center"}>Get the Instagram app.</Box>
             <Flex gap={"5"} justifyContent={"center"}>
               <Image src="/playstore.png" h={"10"} alt="playstore app link" />
-              <Image
-                borderRadius={"md"}
-                src="/microsoft.png"
-                h={"10"}
-                alt="microsoft app link"
-              />
+              <Image src="/microsoft.png" h={"10"} alt="microsoft app link" />
             </Flex>
           </VStack>
         </Flex>
