e3d679820682a03c60cad61b45c4d9c0d54c4b2c
Jasbir Singh <jasbir212singh@gmail.com>
2026-05-25 12:09:50 +0530

    added profile posts to profile page

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,9 +1,48 @@
-import { GridItem } from "@chakra-ui/react";
+import { GridItem, Flex, Text, Image } from "@chakra-ui/react";
+import { FaComment } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
+``;
 
 const ProfilePost = ({ img }) => {
   return (
-    <GridItem>
-      <img src={img} alt="Profile" />
+    <GridItem
+      cursor={"pointer"}
+      borderRadius={"4"}
+      overflow={"hidden"}
+      border={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      position={"relative"}
+      aspesRatio={"1/1"}
+    >
+      <Flex
+        opacity={"0"}
+        _hover={{ opacity: "1" }}
+        position={"absolute"}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        justifyContent={"center"}
+        bg={"blackAlpha.700"}
+        zIndex={1}
+        transition={"all 0.3s ease"}
+      >
+        <Flex alignItems={"center"} gap={50} justifyContent={"center"}>
+          <Flex>
+            <AiFillHeart size={20} />
+            <Text fontWeight={"bold"} ml={2}>
+              7
+            </Text>
+          </Flex>
+          <Flex>
+            <FaComment size={20} />
+            <Text fontWeight={"bold"} ml={2}>
+              7
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Image src={img} alt="Post" w={"100%"} h={"100%"} objectFit={"cover"} />
     </GridItem>
   );
 };
