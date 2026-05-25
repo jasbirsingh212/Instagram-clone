a1b54cc29a68dd5598f10f1163760739d1ef89ac
Jasbir Singh <jasbir212singh@gmail.com>
2026-05-25 13:25:29 +0530

    added Modal to show each post with comments

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -10,10 +10,12 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
+  Box,
+  Avatar,
 } from "@chakra-ui/react";
 import { FaComment } from "react-icons/fa";
 import { AiFillHeart } from "react-icons/ai";
-``;
+import { MdDelete } from "react-icons/md";
 
 const ProfilePost = ({ img }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -40,7 +42,7 @@ const ProfilePost = ({ img }) => {
           justifyContent={"center"}
           bg={"blackAlpha.700"}
           zIndex={1}
-          transition={"all 0.3s ease"}
+          transition={"all 0.2s ease"}
         >
           <Flex alignItems={"center"} gap={50} justifyContent={"center"}>
             <Flex>
@@ -60,13 +62,64 @@ const ProfilePost = ({ img }) => {
         <Image src={img} alt="Post" w={"100%"} h={"100%"} objectFit={"cover"} />
       </GridItem>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={"xl"} isCentered>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size={{ base: "3xl", md: "5xl" }}
+      >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Post Details</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <Image src={img} alt="Post" w={"100%"} h={"100%"} objectFit={"cover"} />
+          <ModalBody bg={"black"} pb={5}>
+            <Flex
+              gap={"4"}
+              mx={"auto"}
+              w={{ base: "90%", sm: "70%", md: "full" }}
+            >
+              <Box
+                borderRadius={"4"}
+                overflow={"hidden"}
+                flex={1.5}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+              >
+                <Image
+                  src={img}
+                  alt="Post"
+                  w={"100%"}
+                  h={"100%"}
+                  objectFit={"cover"}
+                />
+              </Box>
+              <Flex
+                flex={1}
+                flexDirection={"column"}
+                px={10}
+                display={{ base: "none", md: "flex" }}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex alignItems={"center"} gap={4}>
+                    <Avatar
+                      src={"./profilepic.png"}
+                      size={"sm"}
+                      name="asaprogrammer"
+                    />
+                    <Text fontWeight={"bold"} fontSize={"12"}>
+                      asaprogrammer_
+                    </Text>
+                  </Flex>
+                  <Box
+                    _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                    borderRadius={"4"}
+                    p={1}
+                    cursor={"pointer"}
+                  >
+                    <MdDelete size={20} cursor={"pointer"} />
+                  </Box>
+                </Flex>
+              </Flex>
+            </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
